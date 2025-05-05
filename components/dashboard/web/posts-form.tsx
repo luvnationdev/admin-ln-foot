7ffde08ccc19f2e8031424979853ee0b849c4653
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-05-05 19:41:53 +0100

    feat: add PostEditor and PostsForm components; implement custom Checkbox component

---
+++ components/dashboard/web/posts-form.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import type React from 'react'
+
+import PostEditor from '@/components/Posts/PostEditor'
+
+export default function PostsForm() {
+
+  return (
+    <div className='w-full'>
+      <PostEditor />
+    </div>
+  )
+}
